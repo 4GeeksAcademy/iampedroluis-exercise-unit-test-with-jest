133d650757e6d69ef46cda7b1b3e60aa5de52ce9
Pedro Luis Gutierrez <83560861+iampedroluis@users.noreply.github.com>
2023-08-10 19:58:50 +0000

    upload

--- app.js
+++ app.js
@@ -5,36 +5,36 @@ const sum = (a,b)=>{
 ///console.log(sum(7,3))
 
 
-
 let oneEuroIs = {
     "JPY": 127.9, // japan yen
     "USD": 1.2, // us dollar
     "GBP": 0.8, // british pound
 }
 
-function fromDollarToYen(){
-    let dollar = oneEuroIs.USD
+function fromDollarToYen(usd){
+    // Pasar de dolar a euro
+    let euros = usd / oneEuroIs.USD
     let yen = oneEuroIs.JPY
-    let total = yen * dollar
+    let total = yen * euros
 
     return total
 }
-console.log(fromDollarToYen())
+
 
 function fromEuroToDollar(euro){
     let dollar = oneEuroIs.USD
-    let total = (euro * dollar)
+    let total = euro * dollar
     return total
 }
-console.log(" fromEuroToDollar "+fromEuroToDollar(1))
 
-function fromYenToPound(){
-    let yen = oneEuroIs.JPY
+
+function fromYenToPound(yen){
+    let euros = yen / oneEuroIs.JPY
     let pound = oneEuroIs.GBP
-    let total= pound * yen
+    let total= pound * euros
     return total
     
 }
-console.log(fromYenToPound(1))
 
-module.exports = {sum , fromEuroToDollar}
+
+module.exports = {sum , fromEuroToDollar, fromDollarToYen, fromYenToPound}
--- test.js
+++ test.js
@@ -5,8 +5,23 @@ test('adds 14 + 9 to equal 23', ()=>{
     expect(total).toBe(23)
 })
 
-test('one euro Should be 1.206 dollars', function(){
+test('one euro Should be 1.2 dollars', function(){
     const { fromEuroToDollar } = require('./app.js')
-    
-    expect(fromEuroToDollar(3.5)).toBe(4.2)
+    const euro = 3.5
+    const expected = euro * 1.2
+    expect(fromEuroToDollar(euro)).toBe(expected)
+})
+
+test('one dolar Should be', function(){
+    const { fromDollarToYen } = require('./app.js')
+    const dollar = 3.5
+    const expected = 127.9*(dollar/1.2)
+    expect(fromDollarToYen(dollar)).toBe(expected)
+})
+
+test('one dolar Should be 1', function(){
+    const { fromYenToPound } = require('./app.js')
+    const yen = 3.5
+    const expected = 0.8 * (yen / 127.9)
+    expect(fromYenToPound(yen)).toBe(expected)
 })
